ad44aa83835c029e5c890a8a523eb3e7a9e869b3
sl4derxx-spec <sl4derxx@gmail.com>
2026-04-20 22:19:09 +0500

    Add files via upload

--- state.js
+++ state.js
@@ -1,18 +1,6 @@
 export const state = {
-    ngt_stars: 0, 
-    coins: 0,
-    // Изначально рынок пуст
-    currencies: [] 
+    "ngt_stars": 0,
+    "coins": 0,
+    "currencies": []
 };
 
-// Функция для добавления новой валюты (будет вызываться ботом или админкой)
-export const addCurrency = (symbol, startPrice, initialSupply) => {
-    const newCoin = {
-        symbol: symbol.toUpperCase(),
-        price: startPrice,
-        change: 0,
-        supply: initialSupply,
-        history: [startPrice] // Для будущего графика
-    };
-    state.currencies.push(newCoin);
-};
