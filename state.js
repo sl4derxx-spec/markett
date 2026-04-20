5fbff32717f8d96ea7583b7955d361bf20c148b2
sl4derxx-spec <sl4derxx@gmail.com>
2026-04-20 23:14:37 +0500

    Add files via upload

--- state.js
+++ state.js
@@ -1,6 +1,21 @@
 export const state = {
     "ngt_stars": 0,
     "coins": 0,
-    "currencies": []
+    "currencies": [
+        {
+            "symbol": "NGR",
+            "price": 1.2,
+            "supply": 7500,
+            "icon": "🛡️",
+            "history": [1.1, 1.15, 1.2]
+        },
+        {
+            "symbol": "DGI",
+            "price": 73.6,
+            "supply": 7500,
+            "icon": "🔰",
+            "history": [70.0, 75.2, 73.6]
+        }
+    ]
 };
 
