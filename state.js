d37ab7eb70a1532ae2e17e2987276debf4ea0c6a
sl4derxx-spec <sl4derxx@gmail.com>
2026-04-20 22:20:09 +0500

    Бот: Добавлена монета NGR

--- state.js
+++ state.js
@@ -1,6 +1,11 @@
 export const state = {
     "ngt_stars": 0,
     "coins": 0,
-    "currencies": []
+    "currencies": [
+        {
+            "symbol": "NGR",
+            "price": 1.2,
+            "supply": 7500
+        }
+    ]
 };
-
